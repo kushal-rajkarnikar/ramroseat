3f27f5b047aa081d47ed16b2520411fd10d851ad
Kushal Rajkarnikar <es.kushal.web@gmail.com>
2018-11-15 11:57:20 +0545

    Updates

---
+++ precache-manifest.9d0d2425a18c0b5ecd9accbfd168cde1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ramroseat/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "8fd64a888f6009a1299c",
+    "url": "/ramroseat/static/css/main.d798bfe9.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ramroseat/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "ee7ff3a5d93559e9fd54",
+    "url": "/ramroseat/static/js/1.ee7ff3a5.chunk.js"
+  },
+  {
+    "revision": "56d9984a17fcfa1b1291",
+    "url": "/ramroseat/static/js/runtime~main.56d9984a.js"
+  },
+  {
+    "revision": "8fd64a888f6009a1299c",
+    "url": "/ramroseat/static/js/main.8fd64a88.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ramroseat/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ramroseat/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ramroseat/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "ee7ff3a5d93559e9fd54",
+    "url": "/ramroseat/static/css/1.d04ec7c0.chunk.css"
+  },
+  {
+    "revision": "108e7837377f456d83a2e6fd14b298ec",
+    "url": "/ramroseat/index.html"
+  }
+];
